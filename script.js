d49c0bd8d291c2794b3243f65adcb980dcbf785b
JC <jannclementA@gmail.com>
2025-01-27 16:52:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// see more button
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+// nav
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "nav") {
